38a042c01b4555d2cad69d4084f63fa38a68ac1c
Namgyu11 <hagaji3@naver.com>
2024-01-31 16:41:49 +0900

    [feat]: 회원가입, 로그인 및 메일인증 Controller 구현

---
+++ src/main/java/com/example/moviemate/auth/web/AuthController.java
@@ -0,0 +1,45 @@
+package com.example.moviemate.auth.web;
+
+import com.example.moviemate.auth.dto.SignInDto;
+import com.example.moviemate.auth.dto.SignUpDto;
+import com.example.moviemate.auth.service.AuthService;
+import com.example.moviemate.global.util.dto.SendMailRequest;
+import com.example.moviemate.global.util.dto.VerifyMailRequest;
+import com.example.moviemate.global.util.service.MailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+
+  private final AuthService authService;
+  private final MailService mailService;
+
+  @PostMapping("/signUp")
+  public ResponseEntity<?> signUpUser(@RequestBody SignUpDto request){
+    return ResponseEntity.status(HttpStatus.CREATED).body(authService.signUp(request));
+  }
+
+  @PostMapping("/signIn")
+  public ResponseEntity<?> signInUser(@RequestBody SignInDto request){
+    authService.signIn(request);
+    return ResponseEntity.status(HttpStatus.OK).build();
+  }
+  @PostMapping("/mail/certification")
+  public ResponseEntity<?> sendCertificationMail(@RequestBody SendMailRequest request){
+    return ResponseEntity.status(HttpStatus.CREATED)
+        .body(mailService.sendAuthMail(request.getEmail()));
+  }
+  @PostMapping("/mail/verify")
+  public ResponseEntity<?> sendVerifyMail(@RequestBody VerifyMailRequest request){
+    mailService.verifyEmail(request.getEmail(), request.getCode());
+    return ResponseEntity.status(HttpStatus.OK).build();
+  }
+}
