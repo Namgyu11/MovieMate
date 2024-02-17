61d089027186ef4e06e7969a5c12fabe79486630
Namgyu11 <hagaji3@naver.com>
2024-02-18 03:31:35 +0900

    [feat]: 영화 상세 정보 Service 구현

--- src/main/java/com/example/moviemate/MovieMateApplication.java
+++ src/main/java/com/example/moviemate/MovieMateApplication.java
@@ -2,10 +2,11 @@ package com.example.moviemate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class MovieMateApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MovieMateApplication.class, args);
     }
